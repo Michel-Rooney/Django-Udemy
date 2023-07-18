0a67324b0ff04de11b7e4234b44d8b624fb5032d
Michel-Rooney <michelrooney16@gmail.com>
2023-07-18 12:57:59 -0300

    Seção 22: Criando relações com o User Model do Django

--- Project/authors/admin.py
+++ Project/authors/admin.py
@@ -1,4 +1,8 @@
-# flake8: noqa
 from django.contrib import admin
 
-# Register your models here.
+from authors.models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    ...
